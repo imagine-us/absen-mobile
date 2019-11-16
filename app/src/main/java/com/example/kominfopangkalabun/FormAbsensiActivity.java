b037b70774934dbcbee307fa5c970fb3ced82d31
dhyk <dikarizky66@gmail.com>
2019-11-16 12:48:00 +0700

    dika1247

--- app/src/main/java/com/example/kominfopangkalabun/FormAbsensiActivity.java
+++ app/src/main/java/com/example/kominfopangkalabun/FormAbsensiActivity.java
@@ -3,12 +3,29 @@ package com.example.kominfopangkalabun;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class FormAbsensiActivity extends AppCompatActivity {
 
+    ImageView profil, absensi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_absensi);
+
+        profil = findViewById(R.id.imageProfilAbsensi);
+        Picasso.with(this).load(R.drawable.rudiantara).transform(new PicassoCircleTransformation()).into(profil);
+
+        absensi = findViewById(R.id.iconAmbilPresensi);
+        absensi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
