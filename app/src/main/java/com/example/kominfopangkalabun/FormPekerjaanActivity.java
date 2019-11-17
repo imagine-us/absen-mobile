f886ca81327ff9bf5d964c6d0e896ef16c45332d
Yudistira Eka Putra <38902040+yudisputra@users.noreply.github.com>
2019-11-17 09:02:00 +0700

    Form Pekerjaan 16/11/2019 12:37

--- app/src/main/java/com/example/kominfopangkalabun/FormPekerjaanActivity.java
+++ app/src/main/java/com/example/kominfopangkalabun/FormPekerjaanActivity.java
@@ -2,17 +2,32 @@ package com.example.kominfopangkalabun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.icu.util.Calendar;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class FormPekerjaanActivity extends AppCompatActivity {
 
-    EditText edtTanggalKerja, edtJamKerja;
+    EditText edtTanggalKerja, edtJamKerja, edtHasilKerja;
+    Button btnKembaliPekerjaan;
+    private DatePickerDialog datePickerDialog;
+    private TimePickerDialog timePickerDialog;
+    SimpleDateFormat date = new SimpleDateFormat("dd.MM.yyyy");
+    SimpleDateFormat time = new SimpleDateFormat("HH:mm");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +36,30 @@ public class FormPekerjaanActivity extends AppCompatActivity {
 
         edtTanggalKerja = findViewById(R.id.edtTanggalKerja);
         edtJamKerja = findViewById(R.id.edtJamKerja);
+        edtHasilKerja = findViewById(R.id.edtHasilPekerjaan);
+        btnKembaliPekerjaan = findViewById(R.id.btnKembaliPekerjaan);
 
         //set tanggal dan waktu sekarang
-        SimpleDateFormat date = new SimpleDateFormat("dd.MM.yyyy");
         String currentDate = date.format(new Date());
         edtTanggalKerja.setText(currentDate);
 
-        SimpleDateFormat time = new SimpleDateFormat("HH:mm");
         String currentTime = time.format(new Date());
         edtJamKerja.setText(currentTime);
+
+        //persiapa datepciker dan timepicker
+        edtTanggalKerja.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDateDialog();
+            }
+        });
+        
+        edtJamKerja.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showTimeDialog();
+            }
+        });
 
         //generate spinner uraian tugas
         Spinner spinnerUraianTugas = findViewById(R.id.sprUraianTugas);
@@ -41,5 +71,44 @@ public class FormPekerjaanActivity extends AppCompatActivity {
                 android.R.layout.simple_spinner_item, items);
 
         spinnerUraianTugas.setAdapter(adapter);
+
+        btnKembaliPekerjaan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    private void showTimeDialog() {
+
+        Calendar calendar = Calendar.getInstance();
+
+        timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+                edtJamKerja.setText(hourOfDay+":"+minute);
+            }
+        },calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE),DateFormat.is24HourFormat(this));
+        timePickerDialog.show();
+    }
+
+    private void showDateDialog() {
+
+        Calendar newCalendar = Calendar.getInstance();
+
+        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+
+                edtTanggalKerja.setText(date.format(newDate.getTime()));
+            }
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+        datePickerDialog.show();
     }
 }
