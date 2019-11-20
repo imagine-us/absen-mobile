811bf71b62bb03a7d713f2646cf1cf74dfd8eaef
shulhankhairy <al.khair.21@gmail.com>
2019-11-20 19:37:19 +0700

    menambah servis: 1. cek histori absensi 2. cek absensi (bisa apa nggaknya) 3. insert data absensi

--- app/src/main/java/com/example/kominfopangkalabun/retrofit/BaseApiService.java
+++ app/src/main/java/com/example/kominfopangkalabun/retrofit/BaseApiService.java
@@ -4,21 +4,41 @@ import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface BaseApiService {
 
     // Fungsi ini untuk memanggil API
     @FormUrlEncoded
-    @POST("login")
+    @GET("login")
     Call<ResponseBody> loginRequest(@Field("nip") String nip,
                                     @Field("password") String password);
 
+    // mengambil histori absen luar
+    @FormUrlEncoded
+    @GET("absensihist")
+    Call<ResponseBody> getHistori(@Field(("id")) String id);
+
+    // mencek apakah bisa absen saat ini
+    @FormUrlEncoded
+    @GET("cekabsen")
+    Call<ResponseBody> cekAbsen(@Field(("id")) String id);
+
+    // memasukkan data absensi
+    @FormUrlEncoded
+    @POST("absensi")
+    Call<ResponseBody> insertAbsen(@Field("statusid") String statusid,
+                                   @Field("pnsid") String pnsid,
+                                   @Field("latitude") String latitude,
+                                   @Field("longitude") String longitude);
 
     // Fungsi ini untuk memanggil API
     @FormUrlEncoded
     @POST("insert_task.php")
     Call<ResponseBody> insertTask(@Field("judul") String judul,
-                                    @Field("detail") String detail);
+                                  @Field("detail") String detail);
+
+
 
 }
