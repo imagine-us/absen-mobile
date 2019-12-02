232b2e88c92e9494d01be1604b798aa634fda80b
Yudistira Eka Putra <38902040+yudisputra@users.noreply.github.com>
2019-12-02 15:42:12 +0700

    masuk pekerjaan

--- app/src/main/java/com/example/kominfopangkalabun/MainActivity.java
+++ app/src/main/java/com/example/kominfopangkalabun/MainActivity.java
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this,"Fitur belum tersedia", Toast.LENGTH_LONG).show();
-                //startActivity(new Intent(MainActivity.this,FormPekerjaanActivity.class));
+                startActivity(new Intent(MainActivity.this,PekerjaanBaruActivity.class));
             }
         });
 
--- app/src/main/java/com/example/kominfopangkalabun/PekerjaanBaruActivity.java
+++ app/src/main/java/com/example/kominfopangkalabun/PekerjaanBaruActivity.java
@@ -3,12 +3,35 @@ package com.example.kominfopangkalabun;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PekerjaanBaruActivity extends AppCompatActivity {
+
+    Button btnpekerjaanSemua, btnpekerjaanDiterima, btnpekerjaanDitolak, btnpekerjaanBelumDikoreksi, btnKembali;
+    View vwpekerjaanSemua, vwpekerjaanDiterima, vwpekerjaanDitolak, vwpekerjaanBelumDikoreksi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pekerjaan_baru);
+
+
+        btnKembali = findViewById(R.id.btnBackMonitoring);
+        btnpekerjaanSemua = findViewById(R.id.daftarPekerjaanAll);
+        btnpekerjaanDiterima = findViewById(R.id.daftarPekerjaanDiterima);
+        btnpekerjaanDitolak = findViewById(R.id.daftarPekerjaanDitolak);
+        btnpekerjaanBelumDikoreksi = findViewById(R.id.daftarPekerjaanBelumDikoreksi);
+        vwpekerjaanSemua = findViewById(R.id.viewPekerjaanAll);
+        vwpekerjaanDiterima = findViewById(R.id.viewPekerjaanDiterima);
+        vwpekerjaanDitolak = findViewById(R.id.viewPekerjaanDitolak);
+        vwpekerjaanBelumDikoreksi = findViewById(R.id.viewPekerjaanBelumDikoreksi);
+
+        btnKembali.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
