57536d78f34fc5e6b62d887c6d48eb787f1c9e88
Yudistira Eka Putra <38902040+yudisputra@users.noreply.github.com>
2019-12-02 16:49:38 +0700

    fragment pekerjaan selesai

--- app/src/main/java/com/example/kominfopangkalabun/FragmentPekerjaanAll.java
+++ app/src/main/java/com/example/kominfopangkalabun/FragmentPekerjaanAll.java
@@ -1,4 +1,27 @@
 package com.example.kominfopangkalabun;
 
-public class FragmentPekerjaanAll {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.kominfopangkalabun.retrofit.BaseApiService;
+
+public class FragmentPekerjaanAll extends Fragment {
+    BaseApiService mApiService;
+    View v;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        return super.onCreateView(inflater, container, savedInstanceState);
+
+        v = inflater.inflate(R.layout.fragment_pekerjaan_baru,container,false);
+
+        return v;
+    }
 }
--- app/src/main/java/com/example/kominfopangkalabun/PekerjaanBaruActivity.java
+++ app/src/main/java/com/example/kominfopangkalabun/PekerjaanBaruActivity.java
@@ -1,6 +1,9 @@
 package com.example.kominfopangkalabun;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
@@ -33,5 +36,105 @@ public class PekerjaanBaruActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        pekerjaanSemuaActive();
+
+        try{
+            if(getIntent().getExtras().getString("flag").equals("pekerjaanSemua")) pekerjaanSemuaActive();
+        }catch(Exception e){
+
+        }
+
+        btnpekerjaanSemua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pekerjaanSemuaActive();
+            }
+        });
+
+        btnpekerjaanDiterima.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pekerjaanDiterimaActive();
+            }
+        });
+
+        btnpekerjaanDitolak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pekerjaanDitolakActive();
+            }
+        });
+
+        btnpekerjaanBelumDikoreksi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pekerjaanBelumDikoreksiActive();
+            }
+        });
+
+    }
+
+    private void loadFragment(Fragment fragment){
+        FragmentManager fm;
+        FragmentTransaction ft;
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        ft.replace(R.id.frameFragmentPekerjaan, fragment);
+        ft.addToBackStack(null);
+        ft.commit();
+
+    }
+
+    private void pekerjaanSemuaActive() {
+        vwpekerjaanBelumDikoreksi.setVisibility(View.INVISIBLE);
+        vwpekerjaanDitolak.setVisibility(View.INVISIBLE);
+        vwpekerjaanDiterima.setVisibility(View.INVISIBLE);
+        vwpekerjaanSemua.setVisibility(View.VISIBLE);
+        btnpekerjaanSemua.setTextColor(getResources().getColor(R.color.merah));
+        btnpekerjaanBelumDikoreksi.setTextColor(getResources().getColor(R.color.black));
+        btnpekerjaanDitolak.setTextColor(getResources().getColor(R.color.black));
+        btnpekerjaanDiterima.setTextColor(getResources().getColor(R.color.black));
+
+        loadFragment(new FragmentPekerjaanAll());
+    }
+
+    private void pekerjaanDiterimaActive() {
+        vwpekerjaanBelumDikoreksi.setVisibility(View.INVISIBLE);
+        vwpekerjaanDitolak.setVisibility(View.INVISIBLE);
+        vwpekerjaanDiterima.setVisibility(View.VISIBLE);
+        vwpekerjaanSemua.setVisibility(View.INVISIBLE);
+        btnpekerjaanSemua.setTextColor(getResources().getColor(R.color.black));
+        btnpekerjaanBelumDikoreksi.setTextColor(getResources().getColor(R.color.black));
+        btnpekerjaanDitolak.setTextColor(getResources().getColor(R.color.black));
+        btnpekerjaanDiterima.setTextColor(getResources().getColor(R.color.merah));
+
+        loadFragment(new FragmentPekerjaanDiterima());
+    }
+
+    private void pekerjaanDitolakActive() {
+        vwpekerjaanBelumDikoreksi.setVisibility(View.INVISIBLE);
+        vwpekerjaanDitolak.setVisibility(View.VISIBLE);
+        vwpekerjaanDiterima.setVisibility(View.INVISIBLE);
+        vwpekerjaanSemua.setVisibility(View.INVISIBLE);
+        btnpekerjaanSemua.setTextColor(getResources().getColor(R.color.black));
+        btnpekerjaanBelumDikoreksi.setTextColor(getResources().getColor(R.color.black));
+        btnpekerjaanDitolak.setTextColor(getResources().getColor(R.color.merah));
+        btnpekerjaanDiterima.setTextColor(getResources().getColor(R.color.black));
+
+        loadFragment(new FragmentPekerjaanDitolak());
+    }
+
+    private void pekerjaanBelumDikoreksiActive() {
+        vwpekerjaanBelumDikoreksi.setVisibility(View.VISIBLE);
+        vwpekerjaanDitolak.setVisibility(View.INVISIBLE);
+        vwpekerjaanDiterima.setVisibility(View.INVISIBLE);
+        vwpekerjaanSemua.setVisibility(View.INVISIBLE);
+        btnpekerjaanSemua.setTextColor(getResources().getColor(R.color.black));
+        btnpekerjaanBelumDikoreksi.setTextColor(getResources().getColor(R.color.merah));
+        btnpekerjaanDitolak.setTextColor(getResources().getColor(R.color.black));
+        btnpekerjaanDiterima.setTextColor(getResources().getColor(R.color.black));
+
+        loadFragment(new FragmentPekerjaanBelumDikoreksi());
     }
 }
