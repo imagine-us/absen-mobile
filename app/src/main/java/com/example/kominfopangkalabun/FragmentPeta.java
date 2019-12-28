bc9400b81497c8092287bb5edaf280a15362844f
dhyk <dikarizky66@gmail.com>
2019-12-28 15:34:51 +0700

    dika

--- app/src/main/java/com/example/kominfopangkalabun/FragmentPeta.java
+++ app/src/main/java/com/example/kominfopangkalabun/FragmentPeta.java
@@ -350,4 +350,24 @@ public class FragmentPeta extends Fragment {
             }
         });
     }
+
+    public boolean checkJarak(double lat1, double lon1, double lat2, double lon2){
+        final int R = 6371; // Radious of the earth
+        Double latDistance = toRad(lat2-lat1);
+        Double lonDistance = toRad(lon2-lon1);
+        Double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2) +
+                Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
+                        Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        Double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        Double distance = R * c;
+
+        if(distance<=150){ return true;}
+        else {return false;}
+
+
+    }
+
+    private static Double toRad(Double value) {
+        return value * Math.PI / 180;
+    }
 }
