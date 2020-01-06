a0cb31bb7d07353d78b33a4df2907a4097c9b805
Yudistira Eka Putra <38902040+yudisputra@users.noreply.github.com>
2020-01-06 20:35:18 +0700

    Perbaikan Jarak

--- app/src/main/java/com/example/kominfopangkalabun/FragmentPeta.java
+++ app/src/main/java/com/example/kominfopangkalabun/FragmentPeta.java
@@ -131,6 +131,8 @@ public class FragmentPeta extends Fragment {
                        keterangan = "D";
 
                        Boolean cekRadius = checkJarak(Double.parseDouble(doAbsensiList.get(0).getSt_lang()),Double.parseDouble(doAbsensiList.get(0).getSt_long()),lati,longi);
+//                       Double bedajarak =  checkBedaJarak(Double.parseDouble(doAbsensiList.get(0).getSt_lang()),Double.parseDouble(doAbsensiList.get(0).getSt_long()),lati,longi);
+//                       Log.e("Beda Jarak", bedajarak.toString());
                        if(cekRadius){
                            dispatchTakePictureIntent();
                        }
@@ -382,8 +384,21 @@ public class FragmentPeta extends Fragment {
         Double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
         Double distance = R * c;
 
-        if(distance<=150){ return true;}
+        if(distance<=0.1){ return true;}
         else {return false;}
+    }
+
+    public double checkBedaJarak(double lat1, double lon1, double lat2, double lon2){
+        final int R = 6371; // Radious of the earth
+        Double latDistance = toRad(lat2-lat1);
+        Double lonDistance = toRad(lon2-lon1);
+        Double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2) +
+                Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
+                        Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        Double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        Double distance = R * c;
+
+        return  distance;
     }
 
     private static Double toRad(Double value) {
